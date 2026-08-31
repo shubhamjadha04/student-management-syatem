470adef59e4ca6313df451adc2ad89fa983621b3
Shubham Jadhao <shubhamjadhao1124@gmail.com>
2026-08-31 14:20:04 +0530

    add main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+def user_register():
+    try:      
+        name =input("Enter your Name: ")
+        email = input("Enter your Email: ")
+        pws = input("Enter your password: ")
+        role = input("Enter your Role: ")
+        
+
+    except:
+        pass
