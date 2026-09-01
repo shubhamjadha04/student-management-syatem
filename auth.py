c760d0e910f7e079f6b2227081d3fc4cc294cb31
Shubham Jadhao <shubhamjadhao1124@gmail.com>
2026-09-01 14:37:50 +0530

    add auth.py for authentication

---
+++ auth.py
@@ -0,0 +1,76 @@
+from database import cursor,conn
+import mysql.connector
+
+# user register function
+def user_register():
+    try:      
+        name =input("Enter your Name: ")
+        email = input("Enter your Email: ")
+        print("Password should have 8 letters...")
+        pwd = input("Enter your password: ")
+        role = input("Enter your Role: ").lower()
+
+        if len(pwd) < 8:
+            raise ValueError("The Password should have 8 letters... ")
+        
+        if not email.endswith("@gmail.com"):
+            raise ValueError ("Enter valid Email.")
+
+        if role not in ('student', "admin"):
+            raise ValueError ("Enter Valid Role..")
+
+        query = """
+                INSERT INTO users (name,email,password,role)
+                VALUES(%s,%s,%s,%s)
+                 """
+
+        
+
+
+        cursor.execute(query,(name,email,pwd,role))
+        conn.commit()
+
+        user_id = cursor.lastrowid
+
+        print("You have successfully Register.")
+        return user_id
+        
+
+
+# excception handling
+    except ValueError as e:
+        print("Error",e)   
+        return None
+
+    except mysql.connector.Error as e:
+        print("Database error:", e)
+        return None
+
+
+
+# user login function 
+def user_login():
+    try:    
+        email = input("Enter Your email: ")
+        pwd = input("Enter Your password:  ")
+
+        query = """
+                SELECT user_id FROM
+                users WHERE  email = %s and password = %s"""
+
+        cursor.execute(query,(email,pwd))
+
+        user_id = cursor.fetchone()
+
+        if user_id:
+            print("Login Successfull..")
+            print("User Id: ",user_id[0])
+            return user_id[0]
+
+        else:
+            print("Email or Password is wrong..")
+            return None
+
+    except mysql.connector.Error as e:
+        print("Database error:", e)
+        return None
--- main.py
+++ main.py
@@ -1,69 +1,147 @@
 from database import cursor,conn
 import mysql.connector
+from auth import user_login,user_register
 
 
 
-# user register function
-def user_register():
-    try:      
-        name =input("Enter your Name: ")
-        email = input("Enter your Email: ")
-        print("Password should have 8 letters...")
-        pwd = input("Enter your password: ")
-        role = input("Enter your Role: ").lower()
+# # user register function
+# def user_register():
+#     try:      
+#         name =input("Enter your Name: ")
+#         email = input("Enter your Email: ")
+#         print("Password should have 8 letters...")
+#         pwd = input("Enter your password: ")
+#         role = input("Enter your Role: ").lower()
 
-        if len(pwd) < 8:
-            raise ValueError("The Password should have 8 letters... ")
+#         if len(pwd) < 8:
+#             raise ValueError("The Password should have 8 letters... ")
         
-        if not email.endswith("@gmail.com"):
-            raise ValueError ("Enter valid Email.")
+#         if not email.endswith("@gmail.com"):
+#             raise ValueError ("Enter valid Email.")
 
-        if role not in ('student', "admin"):
-            raise ValueError ("Enter Valid Role..")
+#         if role not in ('student', "admin"):
+#             raise ValueError ("Enter Valid Role..")
 
-        query = """
-                INSERT INTO users (name,email,password,role)
-                VALUES(%s,%s,%s,%s)
-                 """
+#         query = """
+#                 INSERT INTO users (name,email,password,role)
+#                 VALUES(%s,%s,%s,%s)
+#                  """
 
-        cursor.execute(query,(name,email,pwd,role))
-        conn.commit()
+#         cursor.execute(query,(name,email,pwd,role))
+#         conn.commit()
 
-        print("You have successfully Register.")
-
-
-# excception handling
-    except ValueError as e:
-        print("Error",e)   
-
-    except mysql.connector.Error as e:
-        print("Database error:", e)
+#         print("You have successfully Register.")
+#         search_student()
 
 
+# # excception handling
+#     except ValueError as e:
+#         print("Error",e)   
 
-# user login function 
-def user_login():
-    try:    
-        email = input("Enter Your email: ")
-        pwd = input("Enter Your password:  ")
+#     except mysql.connector.Error as e:
+#         print("Database error:", e)
 
-        query = """
-                SELECT user_id FROM
-                users WHERE  email = %s and password = %s"""
 
-        cursor.execute(query,(email,pwd))
 
-        user_id = cursor.fetchone()
+# # user login function 
+# def user_login():
+#     try:    
+#         email = input("Enter Your email: ")
+#         pwd = input("Enter Your password:  ")
 
-        if user_id:
-            print("Login Successfull..")
-            print("User Id: ",user_id[0])
+#         query = """
+#                 SELECT user_id FROM
+#                 users WHERE  email = %s and password = %s"""
+
+#         cursor.execute(query,(email,pwd))
+
+#         user_id = cursor.fetchone()
+
+#         if user_id:
+#             print("Login Successfull..")
+#             print("User Id: ",user_id[0])
+#             return True
+
+#         else:
+#             print("Email or Password is wrong..")
+#             return False
+
+#     except mysql.connector.Error as e:
+#         print("Database error:", e)
+
+
+# ADD STUDENT FUNCTION
+def add_student():
+    pass
+
+
+
+# VIEW STUDENT FUNCTION
+def view_student():
+    pass
+
+
+# SEARCH STUDENT FUNCTION
+def search_student():
+    pass
+
+
+# UPDATE STUDENT FUNCTION
+def updat_student():
+    pass
+
+
+# DELETE STUDENT FUNCTION
+def delete_student():
+    pass
+
+
+# LOGOUT STUDENT FUNCTION
+def logout_student():
+    pass
+
+# the student menu function
+
+def student_menu(user_id):
+
+    while True:
+        print("\n"+ "=" *35)
+        print(" STUDENT MANAGEMENT SYSTEM. ")
+        print("="*35)
+        print("1. Add Student.")
+        print("2. View all student.")
+        print("3. Search Student. ")
+        print("4. Update Student.")
+        print("5. Delete Student.")
+        print("6. Logout.")
+        print("7. Exit")
+
+        choice = input("Enter Your Choice: ")
+
+        if choice == "1":
+            add_student()
+
+        elif choice == "2":
+            view_student()
+
+        elif choice == "3":
+            search_student()
+
+        elif choice == "4":
+            updat_student()
+
+        elif choice == "5":
+            delete_student()
+
+        elif choice == "6":
+            logout_student()
+
+        elif choice == "7":
+            print("Exit...")
+            break
 
         else:
-            print("Email or Password is wrong..")
-
-    except mysql.connector.Error as e:
-        print("Database error:", e)
+            print("INVALID OPTION SELECTED.")
 
 
 # the register and the login menu:
@@ -77,10 +155,15 @@ while True:
     choice = input("Enter your choice: ")
 
     if choice == "1":
-        user_register()
+        user = user_register()
+        if user:
+            student_menu(user)
 
     elif choice == "2":
-        user_login()
+        user = user_login()
+        if user:
+            student_menu(user)
+        
 
     elif choice == "3":
         print("Thank you..\nExit")
